9a27aeb24fd705ab39487b5cd0d87c1f8a08889d
JenniferR326 <jennarafael@gmail.com>
2021-03-04 19:55:49 -0500

    added tab content click functions

--- js/main.js
+++ js/main.js
@@ -3,13 +3,26 @@ const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 //Select tab content item
 function selectItem(e) {
-  removeBorder()
+  removeBorder();
+  removeShow();
+  // Add border to current tab
   this.classList.add("tab-border");
+
+  // Grab content from the DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`)
+  // Add show class
+  tabContentItem.classList.add("show")
+
+
 }
 
 function removeBorder() {
-  tabItems.forEach(item => item.classList.remove("tab-border"))
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
 }
 
 //Listen for tab click
-tabItems.forEach(item => item.addEventListener("click", selectItem));
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
